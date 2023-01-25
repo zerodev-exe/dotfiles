196392442b35e3b06d4dc4f2a15fc4ffee5a766d
Zer0 <ivanrodrigues1859@gmail.com>
2023-01-24 23:15:25 -0500

    changed some stuff in the packer config

--- lua/zero/packer.lua
+++ lua/zero/packer.lua
@@ -30,6 +30,11 @@ return require('packer').startup(function(use)
         requires = "nvim-lua/plenary.nvim",
     }
 
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+
 
     use {
         'goolord/alpha-nvim',
